3b9f2fd0446e43108086ceeb172455b1d97ab0ef
lioforest <lioforest@free.fr>
2019-03-31 03:29:16 +0200

    Pas de commentaire... exercice 2

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+print "> "
+user_name = gets.chomp
+puts "Bonjour, c'est quoi ton nom de famille ?"
+print "> "
+user_lastname = gets.chomp
+puts "Bonjour #{user_name} #{user_lastname} !"
---
+++ exo_10.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton année de naissance ?"
+print "> "
+year_born = gets.to_i
+year_reference = 2017
+puts "tu avais : #{year_reference - year_born} ans en #{year_reference}. "
