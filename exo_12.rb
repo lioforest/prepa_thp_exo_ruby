f3943b652d948723048a0a309f1ca22d3d85f818
lioforest <lioforest@free.fr>
2019-03-31 13:05:52 +0200

    exercice 12

---
+++ exo_12.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, merci de me donner un nombre ?"
+print "> "
+number = gets.to_i
+
+n =1
+while n <= number # Tant que n est inférieur ou égal au chiffre demandé, le code est exécuté.
+  puts n
+  n = n + 1 # On ajoute 1 à n à chaque tour pour que sa valeur atteigne le chiffre demandé.
+end
