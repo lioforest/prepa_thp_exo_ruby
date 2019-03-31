6fcbd9423b89cfe6748fc37d8418a03d7534681d
lioforest <lioforest@free.fr>
2019-03-31 01:42:46 +0100

    il calcul le nombre d'heure de travaillé à thp en partant des quantités indiquées dans les variables

--- exo_05.rb
+++ exo_05.rb
@@ -1,22 +1,29 @@
+#affiche le texte
 puts "On va compter le nombre d'heures de travail à THP"
+#affiche le texte suivi du calcul du nombre d'heure travaillé
 puts "Travail : #{10 * 5 * 11}"
+#affiche le texte suivi du nombre d'heure travaillé calculé en minutes
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
-
+#affiche la question
 puts "Et en secondes ?"
-
+#affiche à la ligne suivante nombre d'heure travaillé calculé en secondes
 puts 10 * 5 * 11 * 60 * 60
-
+#affiche la question
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
-
+#affiche à la ligne suivante si l'tiaffimation est vraie ou fausse en calculant les opérations idniqués
 puts 3 + 2 < 5 - 7
-
+#affiche en continue la question suivi du résultat du calcul
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#affiche en continue la question suivi du résultat du calcul
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
-
+#affiche à la ligne le texte
 puts "Ok, c'est faux alors !"
-
+#affiche à la ligne le texte
 puts "C'est drôle ça, faisons-en plus :"
-
+#affiche à la ligne le texte de la question et met  si l'tiaffimation est vraie ou fausse par rapport à l'affirmation mise dans le code
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#affiche à la ligne le texte de la question et met  si l'tiaffimation est vraie ou fausse par rapport à l'affirmation mise dans le code
+
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#affiche à la ligne le texte de la question et met  si l'tiaffimation est vraie ou fausse par rapport à l'affirmation mise dans le code
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"	
---
+++ exo_06.rb
@@ -0,0 +1,5 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
