483d50f61afa78b7e64de345e5a3b3338f03e8f4
lioforest <lioforest@free.fr>
2019-03-31 22:55:32 +0200

    exercice 19

---
+++ exo_19.rb
@@ -0,0 +1,22 @@
+puts "Voici une liste d'Email"
+
+
+
+n = 1
+
+
+
+
+while n <= 50 # Tant que n est inférieur ou égal à 2017, le code est exécuté.
+
+
+if 
+	if n <10 then
+    	puts "jean.dupont.0#{n}@email.fr"  
+
+  	else
+  		puts "jean.dupont.#{n}@email.fr"
+  	end
+
+ n = n + 1 # On ajoute 1 à n à chaque tour pour que sa valeur atteigne le chiffre demandé.
+end
